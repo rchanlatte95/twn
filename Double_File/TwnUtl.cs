b1ed5f58a654ba0aced294ba0cc1429a6f1dedf2
RAC <ryan.a.chanlatte@gmail.com>
2023-08-01 03:47:19 -0400

    Updated tween system
    Optimized evaluation function by only updating the tweens currently active. Tweens not active will be placed at the end of the list and "removed" from the execution queue.
    
    Removed the ability to "reserve" tweens. Instead you use them on demand. The amount available is still invariant; however, you don't need to worry about reserving anything.
    
    Added a check to avoid executing the evaluation loop if the time delta between evaluation ticks is smaller than some arbitrary float.
    
    Removed the multiple arrays that handled the actions. Instead, everything is consolidated into the tween structure.
    
    Made naming more consistent and changed the calling conventions for the functions. Need to use a custom structure and pass a reference to that structure when making calls. Makes calling more explicit and clear while reducing the size of the parameter list.

--- Double_File/TwnUtl.cs
+++ Double_File/TwnUtl.cs
@@ -1,83 +1,114 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+public enum TwnFunc : byte
+{
+    // EO   → Ease Out
+    // EI   → Ease In
+    // EIO  → Ease In/Out
+
+    QUAD_EO = 0,
+    QUAD_EI,
+    QUAD_EIO,
+    CUBIC_EO,
+    CUBIC_EI,
+    CUBIC_EIO,
+    QUART_EO,
+    QUART_EI,
+    QUART_EIO,
+    QUINT_EO,
+    QUINT_EI,
+    QUINT_EIO,
+
+    POW_EO,
+    POW_EI,
+    POW_EIO,
+    CIRC_EO,
+    CIRC_EI,
+    CIRC_EIO,
+    BACK_EO,
+    BACK_EI,
+    BACK_EIO,
+    ELASTIC_EO,
+    ELASTIC_EI,
+    ELASTIC_EIO,
+    BOUNCE_EO,
+    BOUNCE_EI,
+    BOUNCE_EIO,
+    SIN_EO,
+    SIN_EI,
+    SIN_EIO,
+
+    BEZIER,
+    PULSE, // 1 → 0 → 1
+    ESLUP, // 0 → 1 → 0
+    LINEAR = 254,
+    NULL = 255
+}
+
+public enum BOOL : byte { TRUE = 1, FALSE = 0 }
 
 public static class TwnUtl
 {
-    public const float TWN_DUR = 0.404508f; // ϕ/8 = golden ratio divided by eight
-    public const float INV_TWN_DUR = 1.0f / TWN_DUR; // 8/ϕ = eight divided by the golden ratio
+    public const MethodImplOptions INLINE = MethodImplOptions.AggressiveInlining;
+    public const float FLT_EPSILON = 0.0001f;
 
     // Basic tween structure for tweens that have a constant duration.
-    [StructLayout(LayoutKind.Sequential)]
-    public struct constDurTwn // 16 bytes in size
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct constDurTwn // 32 bytes in size
     {
+        // 16 bytes
         public float dt;
         public float strtVal;
         public float dist;
         public state state;
+
+        // 16 bytes
+        public Action onComplete;
+        public Action<float> during;
     }
 
     // Basic tween structure for tweens that have a variable duration.
-    [StructLayout(LayoutKind.Sequential)]
-    public struct twn // 24 bytes in size
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct twn // 40 bytes in size
     {
+        // 24 bytes
         public float dt;
         public float strtVal;
         public float dist;
         public float dur;
         public float invDur;
         public state state;
+
+        // 16 bytes
+        public Action onComplete;
+        public Action<float> during;
+
+        [MethodImpl(INLINE)]
+        public static void Swap(ref twn a, ref twn b, bool preserveID = false)
+        {
+            twn tmp = a;
+            a = b;
+            b = tmp;
+
+            if (preserveID)
+            {
+                // since we just swapped them, swap them back
+                ushort a_ID = b.state.id;
+                ushort b_ID = a.state.id;
+                a.state.id = a_ID;
+                b.state.id = b_ID;
+            }
+        }
     }
-
-    public enum TwnFunc : byte
-    {
-        // EO -> Ease Out
-        // EI -> Ease In
-        // EIO -> Ease In/Out
-        QUAD_EO = 0,
-        QUAD_EI,
-        QUAD_EIO,
-        CUBIC_EO,
-        CUBIC_EI,
-        CUBIC_EIO,
-        QUART_EO,
-        QUART_EI,
-        QUART_EIO,
-        QUINT_EO,
-        QUINT_EI,
-        QUINT_EIO,
-
-        POW_EO,
-        POW_EI,
-        POW_EIO,
-        CIRC_EO,
-        CIRC_EI,
-        CIRC_EIO,
-        BACK_EO,
-        BACK_EI,
-        BACK_EIO,
-        ELASTIC_EO,
-        ELASTIC_EI,
-        ELASTIC_EIO,
-        BOUNCE_EO,
-        BOUNCE_EI,
-        BOUNCE_EIO,
-        SIN_EO,
-        SIN_EI,
-        SIN_EIO,
-
-        BEZIER,
-        LINEAR = 255
-    }
-
-    public const byte FALSE = 0;
-    public const byte TRUE = 1;
 
     [StructLayout(LayoutKind.Explicit)]
     public struct state // 4 bytes in size.
     {
         [FieldOffset(0)] public int full;
-        [FieldOffset(0)] public byte RESERVED;
-        [FieldOffset(1)] public byte RUNNING;
-        [FieldOffset(2)] public TwnFunc TF;
-        [FieldOffset(3)] public byte ID;
+        [FieldOffset(0)] public BOOL RUNNING;
+        [FieldOffset(1)] public TwnFunc EaseFunction;
+        [FieldOffset(2)] public ushort id;
     }
 }
